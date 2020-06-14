1be73dbf437cf3bbe5565c3320465a98ae8c1fac
Dogan Kurt <dogan.kurt@dodobyte.com>
2020-06-14 05:59:11 +0200

    1.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
 	name="dface",
-	version="1.1.2",
+	version="1.1.3",
 	author="deepware",
 	author_email="dogan.kurt@dodobyte.com",
 	description="Face detection and recognition library that focuses on speed and ease of use.",
